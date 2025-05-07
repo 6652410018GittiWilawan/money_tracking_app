a78459e4df4ec6a3abfa9695a1cac70d98207b56
6652410018GittiWilawan <s6652410018@sau.ac.th>
2025-05-07 17:03:15 +0700

    Finish suk tee yark gin mama

--- lib/views/home_ui.dart
+++ lib/views/home_ui.dart
@@ -1,15 +1,235 @@
 import 'package:flutter/material.dart';
+import 'package:money_tracking_app/constants/baseurl_constant.dart';
+import 'package:money_tracking_app/models/money.dart';
+import 'package:money_tracking_app/views/sub_home01_screen.dart';
+import 'package:money_tracking_app/views/sub_home02_screen.dart';
+import 'package:money_tracking_app/views/sub_home03_screen.dart';
+import 'package:money_tracking_app/services/money_api.dart';
 
 class HomeUi extends StatefulWidget {
-  const HomeUi({super.key});
+  final String? userName;
+  final String? userImage;
+  final int? userId;
+
+  const HomeUi({super.key, this.userName, this.userImage, this.userId});
 
   @override
   State<HomeUi> createState() => _HomeUiState();
 }
 
 class _HomeUiState extends State<HomeUi> {
+  int _selectedIndex = 1;
+  late Future<List<Money>> moneyAllData;
+  List showUI = [];
+  Future<List<Money>> getMoneyByUserId() async {
+    return await MoneyApi().getMoneyByUserId(widget.userId!);
+  }
+
+  void refreshData() {
+    setState(() {
+      moneyAllData = getMoneyByUserId(); // Trigger a refresh of the data
+    });
+  }
+
+  @override
+  void initState() {
+    // moneyAllData = getMoneyByUserId();
+    refreshData();
+    showUI = [
+      SubHome01Screen(userId: widget.userId!, refreshData: refreshData),
+      SubHome02Screen(userId: widget.userId!),
+      SubHome03Screen(userId: widget.userId!, refreshData: refreshData),
+    ];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double totalMoney = 0.0;
+    double totalIncome = 0.0;
+    double totalExpanse = 0.0;
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            Text(
+              widget.userName.toString(),
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+        actions: [
+          widget.userImage == null
+              ? Image.asset(
+                'assets/images/user_camera.png',
+                width: 50,
+                height: 50,
+              )
+              : ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.network(
+                  '$baseUrl/images/users/${widget.userImage}',
+                  width: 50,
+                  height: 50,
+                  fit: BoxFit.cover,
+                ),
+              ),
+          SizedBox(width: 25),
+        ],
+        backgroundColor: Colors.brown,
+      ),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(canvasColor: Colors.brown),
+        child: BottomNavigationBar(
+          onTap: (value) {
+            setState(() {
+              _selectedIndex = value;
+            });
+          },
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey[350],
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.arrow_circle_down_outlined, size: 45),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined, size: 45),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.arrow_circle_up_outlined, size: 45),
+              label: '',
+            ),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+        child: Column(
+          children: [
+            FutureBuilder(
+              future: moneyAllData,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  ); // แสดง loading
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('เกิดข้อผิดพลาด: ${snapshot.error}'),
+                  );
+                } else if (snapshot.hasData) {
+                  // ตัวแปรนี้จะทำการคำนวณยอดเงินคงเหลือ
+                  snapshot.data!.forEach((x) {
+                    if (x.moneyType == 1) {
+                      totalMoney += x.moneyInOut!;
+                      totalIncome += x.moneyInOut!;
+                    } else {
+                      totalExpanse += x.moneyInOut!;
+                      totalMoney -= x.moneyInOut!;
+                    }
+                  });
+                  return Container(
+                    width: double.infinity,
+                    height: 200,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 25,
+                      vertical: 25,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.brown,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          "ยอดเงินคงเหลือ",
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                        Text(
+                          "$totalMoney บาท",
+                          style: TextStyle(color: Colors.white, fontSize: 28),
+                        ),
+                        SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.arrow_circle_down_outlined,
+                                      color: Colors.white,
+                                    ),
+                                    SizedBox(width: 5),
+
+                                    Text(
+                                      "ยอดเงินเข้าร่วม",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  "$totalIncome บาท",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      "ยอดเงินออกร่วม",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                    SizedBox(width: 5),
+                                    Icon(
+                                      Icons.arrow_circle_up_outlined,
+                                      color: Colors.white,
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  "$totalExpanse บาท",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                }
+                return Center(child: Text('ไม่มีข้อมูล'));
+              },
+              // childre:
+            ),
+            Expanded(child: showUI[_selectedIndex]),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/custom_textformfield.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class CustomTextFormField extends StatefulWidget {
+  final IconData? iconSuffix;
+  final String labelText;
+  final String hintText;
+  final TextEditingController controller;
+  final Key fieldKey;
+  final String validateText;
+  bool obscureText;
+  CustomTextFormField({
+    super.key,
+    required this.labelText,
+    required this.hintText,
+    required this.controller,
+    required this.fieldKey,
+    required this.validateText,
+    this.iconSuffix,
+    this.obscureText = false,
+  });
+
+  @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  DateTime? selectedDate;
+
+  // ฟังก์ชัน pickDate เพื่อเลือกวันที่
+  Future<void> pickDate() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(1925);
+    final lastDate = DateTime(2026);
+
+    DateTime initialDate = selectedDate ?? now;
+
+    if (initialDate.isAfter(lastDate)) {
+      initialDate = lastDate;
+    }
+
+    DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: initialDate,
+      firstDate: firstDate,
+      lastDate: lastDate,
+    );
+
+    String formatDate(DateTime date) {
+      return "${date.day}-${date.month}-${date.year}";
+    }
+
+    if (picked != null) {
+      setState(() {
+        selectedDate = picked;
+        widget.controller.text = formatDate(
+          picked,
+        ); // อัพเดต TextFormField ด้วยวันที่ที่เลือก
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () {
+            pickDate(); // เปิดเลือกวันที่เมื่อกดที่ TextFormField
+          },
+          child: TextFormField(
+            key: widget.fieldKey,
+            obscureText: widget.obscureText,
+            controller: widget.controller,
+            decoration: InputDecoration(
+              suffixIcon: buildSuffixIcon(),
+              suffixIconColor: Colors.grey,
+              labelText: widget.labelText,
+              labelStyle: TextStyle(color: Colors.black),
+              hintText: widget.hintText,
+              hintStyle: TextStyle(color: Colors.grey),
+              border: const OutlineInputBorder(),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.brown, width: 2.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.brown, width: 2.0),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return widget.validateText;
+              }
+              return null;
+            },
+          ),
+        ),
+        const SizedBox(height: 20),
+      ],
+    );
+  }
+
+  Widget? buildSuffixIcon() {
+    if (widget.iconSuffix != null && widget.iconSuffix == Icons.lock) {
+      return IconButton(
+        icon:
+            widget.obscureText
+                ? Icon(widget.iconSuffix)
+                : Icon(Icons.lock_open),
+        onPressed: () {
+          setState(() {
+            widget.obscureText = !widget.obscureText;
+          });
+        },
+      );
+    } else {
+      return IconButton(
+        icon: Icon(widget.iconSuffix),
+        onPressed: () {
+          pickDate(); // เมื่อกดที่ไอคอนปฏิทินก็เลือกวันที่ได้เช่นกัน
+        },
+      );
+    }
+  }
+}
